f13b8aa760e5c47c54ac3f092f93f151cf570d98
sachinNishalka <72740598+sachinNishalka@users.noreply.github.com>
2023-06-18 02:58:38 +0530

    added spinner to the lugging screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flashchat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'chat_screen.dart';
-
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'loggin_screen';
@@ -14,77 +14,88 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // create a variable to store bool data for spinner
+  bool showSpinner = false;
 
   late String username;
   late String password;
 
-  FirebaseAuth _checkUser =  FirebaseAuth.instance;
-
+  FirebaseAuth _checkUser = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              style: TextStyle(color: Colors.black54),
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                //Do something with the user input.
-                username = value.toString();
-              },
-              decoration:
-                  kInputDecoration.copyWith(hintText: "Enter your email"),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                //Do something with the user input.
-                password = value.toString();
-              },
-              decoration:
-                  kInputDecoration.copyWith(hintText: "Enter your password"),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            paddingButtons(
-              text: 'Log In',
-              func: () async{
-                try {
-                  var user = await _checkUser.signInWithEmailAndPassword(email: username, password: password);
-                  if(user != null){
-                    // print("valid user")
-                    Navigator.pushNamed(context, ChatScreen.id);
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                style: TextStyle(color: Colors.black54),
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  username = value.toString();
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: "Enter your email"),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  password = value.toString();
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: "Enter your password"),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              paddingButtons(
+                text: 'Log In',
+                func: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    var user = await _checkUser.signInWithEmailAndPassword(
+                        email: username, password: password);
+                    if (user != null) {
+                      // print("valid user")
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } on Exception catch (e) {
+                    // TODO
+                    print(e);
                   }
-                } on Exception catch (e) {
-                  // TODO
-                  print(e);
-                }
-              },
-              color: Colors.lightBlueAccent,
-            ),
-          ],
+                },
+                color: Colors.lightBlueAccent,
+              ),
+            ],
+          ),
         ),
       ),
     );
